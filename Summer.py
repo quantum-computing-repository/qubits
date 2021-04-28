813b6bcb6154e95ead0ea130b6fc44a4cd6b3795
Richard Bowmaker <richard.personal@outlook.com>
2021-04-28 22:31:54 +0100

    added in an adder for n qubits

--- Summer.py
+++ Summer.py
@@ -10,8 +10,18 @@ from qiskit.providers.aer import QasmSimulator
 
 def summer():
 
-    addition()
-    subtraction()
+    print("------ Addition -----")
+    for x in range(32):
+        for y in range(32):
+            s = add_n(5, x, y)
+            if s == x + y:
+                print("result ", x, " + ", y, " = ", s)
+            else:
+                print("result ", x, " + ", y, " = ", s, " ****** error *******")
+
+
+#    addition()
+#    subtraction()
 
 def addition():
 # calculate all permutations of (0..7) + (0..7) and display the result
@@ -19,7 +29,7 @@ def addition():
     for x in range(8):
         for y in range(8):
             s = add(x, y)
-            if (s == x + y):
+            if s == x + y:
                 print("result ", x, " + ", y, " = ", s)
             else:
                 print("result ", x, " + ", y, " = ", s, " ****** error *******")
@@ -29,9 +39,9 @@ def subtraction():
     print("------ Subtraction -----")
     for x in range(8):
         for y in range(8):
-            if (x >= y):
+            if x >= y:
                 s = subtract(x, y)
-                if (s == x - y):
+                if s == x - y:
                     print("result ", x, " - ", y, " = ", s)
                 else:
                     print("result ", x, " - ", y, " = ", s, " ****** error *******")
@@ -287,12 +297,79 @@ def set_sum_inverse(circuit, qubits, b1, b2, b3):
     circuit.cx(qubits[b2], qubits[b3])
     circuit.cx(qubits[b1], qubits[b3])
 
+#----------------------------------------------------------------------------------------------
 
+def add_n(n, x, y):
 
+    qr = QuantumRegister(1 + (3*n))
+    cr = ClassicalRegister(1 + (3*n))
 
+    circuit = QuantumCircuit(qr, cr)
 
+    set_x, set_y, get_result = adder_n_get_set(circuit, qr, n)
+    set_x(x)
+    set_y(y)
+    adder_n(circuit, qr, n, 0)
+    circuit.measure(qr, cr)
 
+    simulator = Aer.get_backend('qasm_simulator')
+    result = execute(circuit, simulator, shots=1).result()
+    counts = result.get_counts(circuit)
 
+    r = get_result(counts)
+    return r
+
+def adder_n_get_set(circuit, qubits, n):
+
+    # create list of qubits used to set the input values x and y
+    x_bits = []
+    y_bits = []
+    for m in range(n):
+        x_bits.append(1 + (3 * m))
+        y_bits.append(2 + (3 * m))
+
+    # list of classical bits to read the result
+    r_bits = y_bits.copy()
+    r_bits.append(3 * n)
+
+    # generate and return lambdas to set the input x and y values and return the result
+    set_x = lambda x: set_value(circuit, qubits, x, x_bits)
+    set_y = lambda y: set_value(circuit, qubits, y, y_bits)
+    get_result = lambda counts: get_value(counts, r_bits)
+
+    return set_x, set_y, get_result
+
+# adds an n bit adder to consecutive qubits in a circuit
+# starting at qubit start_bit
+def adder_n(circuit, qubits, n, start_bit):
+# an nbit adder requires n x carry, (n-1) x carry-1, n x sum, 1 x cnot
+# the adder is constructed as follows
+#   n x carry starting at qubit: start_bit + 3m (m = 0..n-1)
+#   1 x cnot at qubit: start_bit + 3n - 2
+#   1 x sum at qubit: start_bit + 3(n-1)
+#   n-1 x carry-1 and sum pair at qubit: start_bit + 3m (m = n-2..0)
+
+    # add n x carry
+    for m in range(n):
+        i = start_bit + (3 * m)
+        set_carry(circuit, qubits, i, i + 1, i + 2, i + 3)
+        circuit.barrier()
+
+    # add cnot
+    i = start_bit + 3 * (n - 1) + 1
+    circuit.cx(qubits[i], qubits[i + 1])
+    circuit.barrier()
+
+    # add sum and carry-1
+    for m in reversed(range(n)):
+        i = start_bit + (3 * m)
+        # don't add carry first time
+        if m < n - 1:
+            set_carry_inverse(circuit, qubits, i, i + 1, i + 2, i + 3)
+            circuit.barrier()
+
+        set_sum(circuit, qubits, i, i + 1, i + 2)
+        circuit.barrier()
 
 
 
